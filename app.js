c0202ef7e2fdd730aed65c010c1bfef59d9378af
David Joshua Reyes <davidjoshuareyes@gmail.com>
2019-03-03 00:39:42 -0500

    Begin process of rebuilding website

--- app.js
+++ app.js
@@ -1,52 +1,41 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
-const indexRouter = require('./routes/index');
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-// instantiate the application
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+const hbs = require( 'express-handlebars');
+const path = require('path');
+const fs = require('fs'); 
+const url = require('url');
+const querystring = require('querystring');
+const http = require('http');
+
+const PORT = 8080;
+
+app.use("/static", express.static(__dirname+'/static'));
+
+app.set('view engine', 'hbs');
+app.engine( 'hbs', hbs( {
+    extname: 'hbs',
+    defaultView: 'default',
+    layoutsDir: __dirname + '/views/pages/',
+    partialsDir: __dirname + '/views/partials/'
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-if(!isProduction) {
-  app.use(logger('common'));
-} else {
-  app.use(logger('dev'));
-}
-
-// handle routes
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/favicon.ico', (req, res) => {
+    console.log('Need to figure out what this is...');
 });
 
-module.exports = app;
+app.get(/^(?!\/api\/)/, (req, res) => {
+    let purl = url.parse(req.url, true);
+    let pathname = 'pages' + purl.pathname;
+    
+    if ((pathname)[pathname.length - 1] === '/') {
+        pathname += 'index';
+    }
+
+    res.render(pathname, purl.query);
+});
