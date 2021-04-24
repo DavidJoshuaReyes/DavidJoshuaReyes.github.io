7a3bcf2435962c5d7c79ae20c7dd910446ceedcd
David Joshua Reyes <davidjoshuareyes@gmail.com>
2021-04-23 20:16:41 -0400

    Some wedding updates

---
+++ src/components/Wedding/welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Welcome = () => {
+    return (
+        <div className="section">
+            <p>
+              <span className="name" style={{ color: '#E7EBEC' }}>Let's celebate together!</span>
+            </p>
+          </div>
+    )
+}
+
+export default Welcome;
